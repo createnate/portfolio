302d035d94a2a049db2e3ce5f4c84d68dfa9226d
Nathan Phillips <nathan@nathanphillips.net>
2019-03-24 12:14:26 -0400

    Initial commit.

---
+++ .eleventy.js
@@ -0,0 +1,46 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("fonts");
+
+  /* Markdown Plugins */
+  // let markdownIt = require("markdown-it");
+  // let markdownItAnchor = require("markdown-it-anchor");
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  let opts = {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  };
+
+  return {
+    templateFormats: [
+      "md",
+      "pug",
+      "html",
+      "liquid"
+    ],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "pug",
+    dataTemplateEngine: "pug",
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+## Implementation Ideas
+
+* Track competitions entered
+* Track workshop weekends
+* Track money spent?
+* Darker color for more hours danced? Pull in color stepper?
