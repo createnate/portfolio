1f0261d5b97670da3fa5e65578157b5adc4d15eb
Nathan Phillips <nathan@nathanphillips.net>
2019-03-24 23:45:38 -0400

    Page titles and favicon.

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
 
   /* Markdown Plugins */
   // let markdownIt = require("markdown-it");
